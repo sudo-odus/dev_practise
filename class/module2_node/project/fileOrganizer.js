20c4f2138b07e82148dac89ecbf10815f8d83bb9
sudo-odus <shubhamshukla477@gmail.com>
2022-03-18 17:00:54 +0530

    file_organizer_project_completed

--- class/module2_node/project/fileOrganizer.js
+++ class/module2_node/project/fileOrganizer.js
@@ -1,11 +1,13 @@
 let fs=require("fs");
 let path=require("path");
 
+
+
 // let input=process.argv;
-let folder_path=process.argv[2];
+let folder_path=process.argv[2];     //path of the folder to be organised taken as input
 // let [,,input]=process.argv; //array destructuring... i like the concept
 console.log(folder_path);
-folderExists=fs.existsSync(folder_path)
+folderExists=fs.existsSync(folder_path);
 
 extensions={
     audio:['.mp3'],
@@ -44,9 +46,9 @@ if(folderExists){
         // console.log("folder for "+element+" is "+give_folder_name(element));
         // code to move file to that folder
         let folder_name=give_folder_name(element);
-        let specific_folder_path=path.join(folder_path,folder_name);
+        let specific_folder_path=path.join(folder_path,folder_name);    //destination folder pathfor the file
         let source_path=path.join(folder_path,element);
-        let destination_path=path.join(folder_path,folder_name,element);
+        let destination_path=path.join(specific_folder_path,element);
         
         // check if folder exists if exists move if does not create the folder
         if(!fs.existsSync(specific_folder_path))
@@ -67,3 +69,10 @@ if(folderExists){
 else{
     console.log("Please enter a valid path");
 }
+
+
+// running perfectly...
+// now to reuse it for new files as well in the code write a if condition for 
+// checking if the element is a file or not .
+// if file then perform the code
+// if not then skip
